8780514d771678f3c083e1e7d69f90f91c2c934c
xixi-1021 <2123752445@qq.com>
2024-09-08 12:36:01 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,13 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int sum = 0;
-	scanf(" %d %d, %a ,&b");
-	sum = a + b;
-	printf("sum = %d\n" sum)
+	int input = 0;
+	 printf("蔡妈妈要不要请我去cicf? (1或0) >: ");                                                                                 //1=请 0=不请
+		scanf("%d/n", &input);
+	if (input == 1)
+		printf("好好好好好好好好好好好好好好好好\n");
+	else
+		printf("不要不要不要打你打你打你\n");
+		
 	return 0;
 }
