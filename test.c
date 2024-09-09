07f8fa0eda7180646c87a2eef4e6e673bdb034c3
xixi-1021 <2123752445@qq.com>
2024-09-09 09:05:09 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,23 +1,142 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
+//
 #include <string.h>
 #include <stdio.h>
-int Max(int x, int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
+//struct SZDX
+//{
+//	char name[20];
+//	char want[20];
+//};
+//int main()
+//{
+//	struct SZDX a1 = { "让詹詹去cicf" ,"报销所有" };
+//	printf("蔡可章要做什么:%s\n", a1.name);
+//	printf("想要的:%s\n", a1.want);
+//	return 0;
+//}
+//int main()
+//{
+//	double d = 3.11;
+//	double* pd = &d;
+//	*pd = 9.9;
+//	printf("%d\n",sizeof( d));
+//	return 0;
+//}
+//
 
 
-}
 
-int main()
-{
-	int num1 = 100;
-	int num2 = 2000;
-	int max = 0;
-	max = Max(num1, num2);
-	printf("max =%d\n", max);
-	return 0;
-}
+
+
+
+
+//#define Max(x,y) (x?y,x:y);
+//int main()
+//{
+//	int a = 233;
+//	int b = 432;
+//	int max = Max(a, b);
+//    printf("max =%d\n" ,max)
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+////#define Max(x,y) (x.y?x:y);
+//int main()
+//{
+//	int a = 76;
+//	int b = 99;
+//	int max = 0;
+//
+//	int max(a . b? a : b);
+//	if (a > b)
+//		printf("%d\n", a);
+//	else
+//		printf("%d\n", b);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+//
+
+
+
+
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//	*pc = 'a';
+//	printf("%c\n" ,ch);
+//	return 0;
+
+
+//#define Max(x,y) (x?y:x,y);
+//int main()
+//{
+//	int a = 100;
+//	int b = 200;
+//	int max = Max(a, b);
+//	printf("max= %d\n", max);
+//	return 0;
+//}
+
+//int main(
+//	{
+//		int a = 100;
+//         
+// 
+//
+//		return 0 
+//	}
+//)
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//
+//
+//}
+//
+//int main()
+//{
+//	int num1 = 100;
+//	int num2 = 2000;
+//	int max = 0;
+//	max = Max(num1, num2);
+//	printf("max =%d\n", max);
+//	return 0;
+//}
+//void test()
+//{
+//	static int a = 2;
+//	a++;
+//	printf("a=%d\n", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i<5)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
