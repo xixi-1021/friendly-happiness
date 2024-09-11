f30f6fbd1d79255088d736eac48f4a258b08112b
xixi-1021 <2123752445@qq.com>
2024-09-11 17:04:52 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -2,7 +2,215 @@
 //
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void menu()
+{
+	printf("**************************\n");
+	printf("**** 1.play    0.exit ****\n");
+	printf("**************************\n");
+}
+void game()
+{
+	int ret = 0;
+	int guess = 0;
+	srand((unsigned int)time(NULL));
+	//时间戳
+	ret = rand()%100+1;
+	while (1)
+	{
+		printf("猜猜数字:>");                              //简单的猜数字游戏
+		scanf("%d", &guess);
+		if (guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else if (guess < ret)
+		{
+			printf("猜小了 \n");
+		}
+		else
+		{
+			printf("恭喜你，猜对了\n");
+			break;
+		}
+	}
+	printf("%d\n", ret);
+}
+int main()
+{
+	int input = 0;
+	do 
+	{
+		menu();
+		printf("请选择>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+
+
+	} while (input);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 1;
+//		for (j = 1; j <= i; j++)                           //打印99乘法表
+//		{
+//			printf("%d*%d=%-2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+// 
+// 
+// 
+// 
+////int main()
+////{
+//	int i = 0;
+//	double sum = 0.0;
+//	int shu = 1;
+//	for (i = 1; i <= 100; i++)                 //算1/1-1/2+.......-1/100
+//	{
+//		sum += shu * 1.0 / i;
+//		shu = -shu;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+// 
+// 
+// 
+// 
+// 
+//}
+////int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int j = 0;
 //
+//		for (j = 2; j < i; j++)                       //打印素数
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//			
+//		}
+//		if (j == i)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+ /*}*/
+//{
+//	int i = 0;
+//	for (i; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//		 printf("%d ", i);               // 3 的公倍数
+//	}
+//	return 0;
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//}
+////int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);                   //求三个数的大到小
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d%d%d", a, b, c);
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz-1;
+//	while(left <= right)
+//	{
+//		int mid = (left + right) / 2;                    //二分法
+//		if (arr[mid] > k)
+//		{
+//			right = mid-1;
+//		}
+//		else if(arr[mid] < k)
+//		{
+//			left = mid+1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是：%d\n", mid);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+////
 //int main()
 ////{
 //	int i = 0;
@@ -22,7 +230,7 @@
 
 //int main()
 //{
-//	int i = 0;
+//	 int i = 0;
 //	do
 //	{
 //		printf("%d\n", i);                       //do while easy
@@ -43,15 +251,24 @@
 //	}
 //	return 0;
 //}
+// 
+// 
+// 
+// 
 //int main()
 //{
 //	int i = 0;
 //	//   初始化  判断    调整
 //	for (i = 1; i <= 10; i++)           //for简单应用
 //	{ 
+// 
+// 
 //		printf("%d", i);
 //	}
 //	return 0;
+// 
+// 
+// 
 //}
 ////int main()
 //{
