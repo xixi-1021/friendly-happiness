a87709eaaabb4b5c1008cd9b0855be0555c37307
xixi-1021 <2123752445@qq.com>
2024-09-14 15:40:10 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -5,52 +5,204 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-
-int binary_search(int arr[], int k, int sz)
-
+int Fib(int n)
 {
-	 
-	int left = 0;
-	int right = sz - 1;
+	int a = 1;
+	int b = 1;
+	int c = 1;
 
-	while (left <= right)
+	while (n > 2)
 	{
-		int mid = (left + right) / 2;               //重要的函数的二分查找
-		if (arr[mid] < k)
-		{
-			left = mid + 1;
-		}
-		else if (arr[mid] > k)
-		{
-			right = mid - 1;
-		}
-		else
-		{
-			return mid;
-		}
-
+		c = a + b;
+		a = b;
+		b = c;                          //递归经常会栈溢出
+		n--;
 	}
-	return -1;
+	return c;                                  //求斐波那契数列更好
+
 }
+
 
 int main()
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int k = 7;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int ret = binary_search(arr, k, sz);
-	if (ret == -1)
-	{
-		printf("找不到指定的数字 \n");
-	}
-	else
-	{
-		printf("找到了，下标是：%d\n", ret);
-	}
-	return 0;
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fib(n);
+	printf("ret = %d\n", ret);
 
+	return 0;
 }
 
+
+//int Fib(int n)
+//{
+//	if (n <=2)
+//	{
+//		return 1;
+//	}
+//	else                                           //斐波那契数列但是不好用
+//	{
+//		return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fib(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+
+
+//
+//
+//int Fac(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac(n - 1);            //递推求阶乘
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
+////int my_strlen(char* str)
+////{
+////	if (*str != '\0')
+////		return 1 + my_strlen(str + 1);                               // 自定义2.0
+//	else          
+//		return 0;
+//}
+//
+//
+//
+//int main()
+//{
+//
+//	char arr[] = "bittttt";
+//	int len = my_strlen(arr);
+//	printf("len = %d\n", len);
+//
+//	return 0;
+//}
+
+
+
+
+
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;                                          //自定义函数
+//		str++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//
+//	char arr[] = "bittttt";
+//	int len = my_strlen(arr);
+//	printf("len = %d\n", len);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);                       //简单递归
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d",  &num);
+//	print(num);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d",printf("%d",printf("43")));                   //.h声明（一般头文件中）    .c定义，   先声名再使用
+//	return 0;
+//}
+
+
+
+
+
+//int binary_search(int arr[], int k, int sz)
+//
+//{
+//	 
+//	int left = 0;
+//	int right = sz - 1;
+//
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;               //重要的函数的二分查找
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k, sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到指定的数字 \n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//
+//}
+//
 
 
 
